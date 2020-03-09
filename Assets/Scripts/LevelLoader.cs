fc68316de7ef152913bc6955ea0d791405b3172e
Bradley Christensen <49649546+bradjc95certitude@users.noreply.github.com>
2020-03-09 10:23:46 -0500

    Gym Tutorial Level Finished

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    const float START_DELAY = 5f;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        Invoke("StartGame", START_DELAY);
+    }
+
+    void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+
+{
+    [Tooltip("In m/s")] [SerializeField] float XSensitivity = 2f;
+    [Tooltip("In m/s")] [SerializeField] float YSensitivity = 2f;
+    [SerializeField] float MaxXPos = 10f;
+    [SerializeField] float YMax = 5f;
+    [SerializeField] float YMin = 5f;
+    [SerializeField] float XPositionYawFactor = 3f;
+    [SerializeField] float YPositionPitchFactor = -2f;
+    [SerializeField] float XThrowYawFactor = 100f;
+    [SerializeField] float YThrowPitchFactor = 100f;
+    [SerializeField] float XPositionRollFactor = 20f;
+
+    [SerializeField] float xThrow = 0f;
+    [SerializeField] float yThrow = 0f;
+
+    Quaternion startingRotation;
+
+    void Start()
+    {
+        startingRotation = transform.localRotation;
+    }
+
+
+    void Update()
+    {
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessTranslation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float dt = Time.deltaTime;
+        float xoffset = xThrow * XSensitivity * dt;
+        float yoffset = yThrow * YSensitivity * dt;
+
+        float xpos = Mathf.Clamp(transform.localPosition.x + xoffset, -MaxXPos, MaxXPos);
+        float ypos = Mathf.Clamp(transform.localPosition.y + yoffset, -YMax, YMin);
+        float zpos = transform.localPosition.z;
+
+        transform.localPosition = new Vector3(xpos, ypos, zpos);
+    }
+
+    void ProcessRotation()
+    {
+        float pitch = startingRotation.eulerAngles.x + transform.localPosition.y * YPositionPitchFactor + yThrow * YThrowPitchFactor;
+        float yaw = startingRotation.eulerAngles.y + transform.localPosition.x * XPositionYawFactor + xThrow * XThrowYawFactor;
+        float roll = startingRotation.eulerAngles.z + xThrow * XPositionRollFactor;
+        transform.localRotation = Quaternion.Euler(roll, yaw, pitch);
+    }
+}
